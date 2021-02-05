2ae556699a8b3eef1a5764995fa5fc4865ec01bc
JASON ROUNDTREE <jasontree123@gmail.com>
2021-02-04 16:04:21 -0800

    Render per-pizza quantity to cart

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -1,7 +1,66 @@
 import React from 'react'
+import { FiShoppingCart } from 'react-icons/fi'
+import CartCSS from './Cart.module.css'
+import { AppStateContext } from './AppState'
 
-class Cart extends React.Component {
-    
+interface Props {}
+
+interface State {
+    isOpen: boolean
+}
+
+class Cart extends React.Component<Props, State> {
+    constructor(props: Props) {
+        super(props)
+        this.state = {
+            isOpen: false
+        }
+    }
+
+    handleClick = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+        // console.log((e.target as HTMLElement))
+        if ((e.target as HTMLElement).nodeName === 'SPAN') {
+            (e.target as HTMLSpanElement)
+        }
+        this.setState((state) => {
+            return { isOpen: !state.isOpen }
+    })}
+
+    render() {
+        return (
+            <AppStateContext.Consumer>
+                {(state) => {
+                    return (
+                        <div className={CartCSS.cartContainer}>
+                            <button 
+                                className={CartCSS.button} 
+                                type="button"
+                                onClick={this.handleClick}
+                            >
+                                <FiShoppingCart />
+                                <span>{state.cart.items.length} pizza(s)</span>
+                            </button>
+                            <div 
+                                className={CartCSS.cartDropDown}
+                                style={{display: this.state.isOpen ? 'block' : 'none'}}
+                            >
+                                <ul>
+                                    {state.cart.items.map(item => {
+                                        return (
+                                            <li key={item.id}>
+                                                {item.name} &nbsp;
+                                                &times; {item.quantity}
+                                            </li>
+                                        )
+                                    })}
+                                </ul>
+                            </div>
+                        </div>
+                    )
+                }}
+            </AppStateContext.Consumer>
+        )
+    }
 }
 
 export default Cart
